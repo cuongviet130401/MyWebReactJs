f27aff2383bec925db6c00b41da313c809344550
cuongviet130401 <cuongviet130401@gmail.com>
2022-10-14 01:51:06 +0700

    Hutao

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+/* eslint-disable jsx-a11y/iframe-has-title */
 import './App.css';
-
+import avt from '../src/assets/images/IMG_1096.JPG';
+import PostForm from './API/PostForm'
+import PostList from './API/PostList'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <header className="App-header">
+    <img src={avt} alt="avt"width="400px"height="400px"/>
+    <p>
+    Nguyen Cuong Viet
+    </p>
+    {/*https://www.youtube.com/watch?v=cQhx1Cp811w*/}
+    <h1> Hutao Theme </h1>  
+    <iframe width="400" height="400" src="https://www.youtube.com/embed/cQhx1Cp811w">
+    </iframe>
+    </header>
+    <PostForm></PostForm>
     </div>
   );
 }
